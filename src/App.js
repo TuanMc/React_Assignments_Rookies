04d56fe8afefd3677f163e2b2ba5981c561ef573
Mai Chung Tuấn <maichungtuan@gmail.com>
2022-03-02 15:45:07 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Home from './components/Home'
 import NavBar from './components/NavBar';
 
-function App() {
+function App() { // This should be class component
+  // Create a state with property 'bootcamp'
   return (
     <div className="App">
-      <NavBar username = {'NGUYEN HOANG THANG'} />
-      <Home bootcamp= { 'Rookies'}/>
+      <NavBar username = {'NGUYEN HOANG THANG'} /> <!-- No need to add props here -->
+      <Home bootcamp= { 'Rookies'}/> <!-- Replace this value by this.state.bootcamp -->
     </div>
   );
 }
